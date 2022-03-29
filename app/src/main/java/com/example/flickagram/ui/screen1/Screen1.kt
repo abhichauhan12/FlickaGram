9af990dcbb5e04119808ef7928214a9fc987cfb5
Abhishek <ac1231998@gmail.com>
2022-03-29 08:56:15 +0530

    basic setup done

---
+++ app/src/main/java/com/example/flickagram/ui/screen1/Screen1.kt
@@ -0,0 +1,10 @@
+package com.example.flickagram.ui.screen1
+
+import androidx.fragment.app.Fragment
+import com.example.flickagram.R
+
+
+class Screen1 : Fragment(R.layout.fragment_screen1) {
+
+
+}
---
+++ app/src/main/java/com/example/flickagram/ui/screen2/Screen2.kt
@@ -0,0 +1,10 @@
+package com.example.flickagram.ui.screen2
+
+import androidx.fragment.app.Fragment
+import com.example.flickagram.R
+
+class Screen2 : Fragment(R.layout.fragment_screen2) {
+
+
+
+}
